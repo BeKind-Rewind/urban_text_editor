ae1327fc4e607a83a1fae465e7fbe2c01abd02a9
Amanda Perry <challenge641@gmail.com>
2022-10-06 17:47:21 -0500

    updating serverjs

--- server/server.js
+++ server/server.js
@@ -1,13 +1,29 @@
-const express = require('express');
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const compression = require("compression");
+
+const PORT = process.env.PORT || 3001;
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-app.use(express.static('../client/dist'));
-// app.use(express.static('../client/'));
+app.use(logger("dev"));
+
+app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require('./routes/htmlRoutes')(app);
+app.use(express.static("public"));
 
-app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+// routes
+app.use(require("./routes/api.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
